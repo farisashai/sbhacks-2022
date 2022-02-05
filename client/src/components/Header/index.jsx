ab690ca927f0c3ce68017038ae818fe5df6b3103
Faris Ashai <farisashai@gmail.com>
2022-02-05 15:39:30 -0800

    added client side routing hook

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,20 +1,24 @@
 import './style.less'
 import pen from 'assets/pen.svg'
 import scribble from 'assets/scribble.svg'
+import { useNavigate } from 'react-router-dom'
 
-const Header = ({title, onClick}) => {
-  return <div className="header">
-    <div className="header-left">
-      <img src={pen} alt="pencil" />
-      <h1>Quizlash</h1>
+const Header = ({ title, onClick }) => {
+  const navigate = useNavigate()
+  return (
+    <div className="header">
+      <div onClick={() => navigate('/')} className="header-left">
+        <img src={pen} alt="pencil" />
+        <h1>Quizlash</h1>
+      </div>
+      <div className="header-right">
+        <button className="join-game" onClick={onClick}>
+          {title}
+        </button>
+        <img src={scribble} alt="" />
+      </div>
     </div>
-    <div className="header-right">
-      <button className="join-game" onClick={onClick}>
-        { title }
-      </button>
-      <img src={scribble} alt="" />
-    </div>
-  </div>
-};
+  )
+}
 
-export default Header;
+export default Header
--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,49 +1,47 @@
-import './style.less';
+import './style.less'
 import LandingCircle from 'components/Circle/LandingCircle'
-import HowToCircle from 'components/Circle/HowToCircle';
+import HowToCircle from 'components/Circle/HowToCircle'
 import howto1 from 'assets/howto1.svg'
 import howto2 from 'assets/howto2.svg'
 import howto3 from 'assets/howto3.svg'
 import { useState } from 'react'
-import Layout from 'containers/Layout';
+import Layout from 'containers/Layout'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
-  const [index, setIndex] = useState(0);
+  const [index, setIndex] = useState(0)
+  const navigate = useNavigate()
 
   const slides = [
-    <LandingCircle
-      onclick={() => setIndex(index => index + 1)}
-    />,
-    <HowToCircle 
+    <LandingCircle onclick={() => setIndex((index) => index + 1)} />,
+    <HowToCircle
       image={howto1}
       description="First, upload your notes to generate questions!"
       buttonText="Next"
-      back={() => setIndex(index => index - 1)}
-      next={() => setIndex(index => index + 1)}
+      back={() => setIndex((index) => index - 1)}
+      next={() => setIndex((index) => index + 1)}
     />,
-    <HowToCircle 
+    <HowToCircle
       image={howto2}
       description="Second, have your friends scan the QR code"
       buttonText="Next"
-      back={() => setIndex(index => index - 1)}
-      next={() => setIndex(index => index + 1)}    />,
-    <HowToCircle 
+      back={() => setIndex((index) => index - 1)}
+      next={() => setIndex((index) => index + 1)}
+    />,
+    <HowToCircle
       image={howto3}
       description="Last, answer questions and climb the leaderboard!"
       buttonText="Start"
-      back={() => setIndex(index => index - 1)}
-      next={() => window.location.href = '/upload'}
+      back={() => setIndex((index) => index - 1)}
+      next={() => navigate('/upload')}
     />,
-  ];
-  
+  ]
+
   return (
     <Layout>
-      <div className="circle">
-        {slides[index]}
-      </div>
+      <div className="circle">{slides[index]}</div>
     </Layout>
-      
-  );
+  )
 }
 
-export default Home;
+export default Home
